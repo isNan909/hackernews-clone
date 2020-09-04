538d6a96e228b8cb85b1399941399ead3f76c30c
Ishan Manandhar <ishan02016@gmail.com>
2020-09-04 12:40:10 +0545

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheet/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hacker News Clone with React</div>;
 }
 
 export default App;
